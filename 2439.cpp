afaec9b063d42b59eccc7deb1d9cc690b6efb250
박미라 <parkmira323@gmail.com>
2022-06-18 16:40:37 +0900

    Add files via upload

---
+++ 2439.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+
+int main(void)
+{
+    int N;
+    cin >> N;
+
+    for(int y=0; y<N; y++){
+
+        for(int z = 0; z<(N-y-1); z++){
+            cout << ' ';
+        }
+            for(int x=0; x<y+1; x++){
+                cout << '*';
+            }
+
+            cout << "\n";
+    }
+
+    return 0;
+}
