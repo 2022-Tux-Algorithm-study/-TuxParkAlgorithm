1c28d876c086810485bbce8a7954b30597864d85
박미라 <parkmira323@gmail.com>
2022-07-19 19:01:08 +0900

    Add files via upload

---
+++ 2022.07.19.TUE/2742.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int main(void)
+{
+    int N;
+    cin >> N;
+
+    for(int i=N;i>0;i--){
+        cout << i << "\n";
+    }
+
+
+
+
+
+    return 0;
+}
