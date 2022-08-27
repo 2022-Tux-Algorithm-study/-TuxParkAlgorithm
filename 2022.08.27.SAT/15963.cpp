637be1395f4278cd1c95e1385601dd7e4527dfbe
박미라 <parkmira323@gmail.com>
2022-08-27 10:22:34 +0900

    Add files via upload

---
+++ 2022.08.27.SAT/15963.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main(void)
+{
+    long long N,M;
+    cin >> N >> M;
+
+    if(N == M){
+        cout << 1 << "\n";
+        return 0;
+
+    }
+
+    else{
+        cout << 0 << "\n";
+        return 0;
+    }
+}
